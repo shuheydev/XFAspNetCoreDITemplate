69dca135986aeb624236d71ed8b926e73de9403e
Shuhey <shuhey.dev@gmail.com>
2020-06-18 19:56:39 +0900

    initial commit

---
+++ XFAspNetCoreDITemplate/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace $safeprojectname$
+{
+    public partial class App : Application
+    {
+        public App(Page startupPage)
+        {
+            InitializeComponent();
+
+            MainPage = startupPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

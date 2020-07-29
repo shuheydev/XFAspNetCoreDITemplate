77de928cd86a05aa519c5490e6e3289883c6fd9f
shuheydev <shuhey.dev@gmail.com>
2020-07-29 23:33:15 +0900

    前回のアップデート時にAndroid,iOSのプロジェクト名を変更するのを忘れていた.修正.

--- XFAspNetCoreDITemplate.Android/MainActivity.cs
+++ XFAspNetCoreDITemplate.Android/MainActivity.cs
@@ -26,7 +26,7 @@ namespace $safeprojectname$
 
             LoadApplication(Startup.Init(ConfigureServices));
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
